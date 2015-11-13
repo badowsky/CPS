d357bc59da11222f7b154ec5f53140e92f80b52f
levil545 <levil545@gmail.com>
2015-11-13 21:16:05 +0100

    Modyfikacja klasy kwantyzacji.

--- src/Model/Konwersja/KwantyzacjaZZaokragleniem.java
+++ src/Model/Konwersja/KwantyzacjaZZaokragleniem.java
@@ -1,5 +1,7 @@
 package Model.Konwersja;
 
+import java.util.TreeSet;
+
 import Model.FunkcjeCiagle.FunkcjaCiagla;
 import Model.Sygnaly.Dyskretne.SygnalDyskretny;
 	
@@ -10,8 +12,42 @@ public class KwantyzacjaZZaokragleniem extends Kwantyzacja {
 	}
 
 	public SygnalDyskretny kwantyzuj(FunkcjaCiagla sygnal, double poczatek, int czestotliwosc, double koniec, int ileStopni) {
+		
+		SygnalDyskretny discreteSignal = Próbkowanie.próbkuj(sygnal, poczatek, czestotliwosc, koniec);
+		
+		double max = discreteSignal.x.get(0);
+		double min = discreteSignal.y.get(0);
+		
+		for (int i=0; i<discreteSignal.y.size(); i++) {
+			
+			if (max < discreteSignal.y.get(i)) {
+				max = discreteSignal.y.get(i);
+			}
+			
+			if (min > discreteSignal.y.get(i)) {
+				min = discreteSignal.y.get(i);
+			}
+		}
+		
+		double sub = max - min;
 
-		return null;
+		TreeSet<Double> treeSet = new TreeSet<Double>();
+		
+		for (int i=0; i < ileStopni; i++) {
+			treeSet.add(min + ((sub / (ileStopni)) * i));
+		}
+	
+		SygnalDyskretny discreteSignalReturn = new SygnalDyskretny();
+		
+		for (int i=0; i<discreteSignal.y.size(); i++) {
+			Double tempX, tempY;
+			tempX = discreteSignal.x.get(i);
+			tempY = treeSet.floor(discreteSignal.y.get(i));
+			discreteSignalReturn.x.add(tempX);
+			discreteSignalReturn.y.add(tempY);
+		}
+		
+		return discreteSignalReturn;
 	}
 	
 	@Override
